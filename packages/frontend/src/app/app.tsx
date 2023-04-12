b99c79b509c15b85a736dd574924477fad0b3089
Davison27 <davidencinarejo27@gmail.com>
2023-04-12 19:11:08 +0200

    feat(frontend): chakra ui componentes library added

--- packages/frontend/src/app/app.tsx
+++ packages/frontend/src/app/app.tsx
@@ -1,18 +1,21 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import './app.scss'
 
+import { ChakraProvider } from '@chakra-ui/react'
 import { Outlet } from 'react-router-dom'
 
 import Navbar from '../components/NavBar/Navbar'
 
 export function App() {
   return (
-    <div className="app">
-      <Navbar />
-      <div className="app-content">
-        <Outlet />
+    <ChakraProvider>
+      <div className="app">
+        <Navbar />
+        <div className="app-content">
+          <Outlet />
+        </div>
       </div>
-    </div>
+    </ChakraProvider>
   )
 }
 
