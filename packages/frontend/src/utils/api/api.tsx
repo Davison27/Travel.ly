5fff8a72458275eb331742b0a66e1b63955fff98
Davison27 <davidencinarejo27@gmail.com>
2023-06-25 18:05:05 +0200

    feat(frontend): activities calls almost done

--- packages/frontend/src/utils/api/api.tsx
+++ packages/frontend/src/utils/api/api.tsx
@@ -62,11 +62,61 @@ const deleteTravel = async (id: string) => {
   return request.data
 }
 
+const getActivities = async (travelId: string) => {
+  const response = await axios.get(`${myApi}/activities/${travelId}`)
+  console.log(response.data)
+  return response.data
+}
+
+const postActivity = async (
+  name: string,
+  description: string,
+  startDate: string,
+  endDate: string,
+  price: number,
+  category: string,
+  documentUrl: string,
+  travelId: string,
+  ActivityId: string,
+) => {
+  const request = await axios.post(`${myApi}/activities/${travelId}`, {
+    ActivityId,
+    category,
+    description,
+    documentUrl,
+    endDate,
+    name,
+    price,
+    startDate,
+    travelId,
+  })
+  console.log(request.data)
+  return request.data
+}
+
+const updateActivity = async (travelId: string) => {
+  const request = await axios.put(`${myApi}/activities/${travelId}`)
+  console.log(request.data)
+  return request.data
+}
+
+const deleteActivity = async (travelId: string) => {
+  const request = await axios.delete(`${myApi}/activities/${travelId}`)
+  console.log(request.data)
+  return request.data
+}
+
+// preguntar a pablo por el delete y el update activity
+
 export default {
+  deleteActivity,
   deleteTravel,
+  getActivities,
   getStatus,
   getTravel,
   getTravels,
+  postActivity,
   postTravel,
+  updateActivity,
   updateTravel,
 }
