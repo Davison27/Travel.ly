8475d7c53e1a95b574b483051d2d971d6f0e4780
Davison27 <davidencinarejo27@gmail.com>
2023-08-25 23:04:06 +0200

    feat(common): end-to-end tests almost completed

--- packages/frontend-e2e/src/e2e/app.cy.ts
+++ packages/frontend-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,98 @@
-import { getGreeting } from '../support/app.po'
+import 'cypress-localstorage-commands'
 
-describe('frontend', () => {
-  beforeEach(() => cy.visit('/'))
+import * as cypress from 'cypress'
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword')
+describe('First access', () => {
+  it('Visits the signIn page', () => {
+    cy.visit('http://localhost:4200/')
+  })
+})
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome frontend')
+describe('List travels', () => {
+  it('Should login and list travels', () => {
+    cy.setLocalStorage('token', JSON.stringify('token'))
+    cy.saveLocalStorage()
+    cy.visit('http://localhost:4200/travels')
+  })
+})
+
+describe('Go to user page', () => {
+  it('should go to user page', () => {
+    cy.setLocalStorage('token', JSON.stringify('token'))
+    cy.visit('http://localhost:4200/user')
+  })
+})
+
+describe('View travel', () => {
+  it('should enter into one travel', () => {
+    cy.setLocalStorage('token', JSON.stringify('token'))
+    cy.visit('http://localhost:4200/travels')
+    cy.contains('Ver').click()
+  })
+})
+
+describe('Create travel', () => {
+  it('should create a new travel', () => {
+    cy.setLocalStorage('token', JSON.stringify('token'))
+    cy.visit('http://localhost:4200/new-travel')
+    cy.get('input[name="name"]').type('Barcelona')
+    cy.get('input[name="description"]').type('Viaje a Barcelona')
+    cy.get('input[name="startDate"]').type('2017-06-01T08:30')
+    cy.get('input[name="endDate"]').type('2017-06-01T08:31')
+    cy.get('input[name="travelers"]').type('10')
+    cy.get('input[name="budget"]').type('1000')
+    cy.contains('Guardar viaje').click()
+  })
+})
+
+describe('Edit travel', () => {
+  it('should edit a travel', () => {
+    cy.setLocalStorage('token', JSON.stringify('token'))
+    cy.visit('http://localhost:4200/travels')
+    cy.contains('Modificar').click()
+    cy.get('input[name="name"]').type('Prueba de viaje')
+    cy.get('input[name="startDate"]').type('2017-06-01T08:30')
+    cy.get('input[name="endDate"]').type('2017-06-01T08:31')
+    cy.contains('Guardar viaje').click()
+  })
+})
+
+describe('Delete travel', () => {
+  it('should delete a travel', () => {
+    cy.setLocalStorage('token', JSON.stringify('token'))
+    cy.visit('http://localhost:4200/travels')
+    cy.contains('Borrar').click()
+    cy.contains('Borrar').click({ force: true })
+  })
+})
+
+describe('Create activity', () => {
+  it('should create an activity inside a travel', () => {
+    cy.setLocalStorage('token', JSON.stringify('token'))
+    cy.visit('http://localhost:4200/travels')
+    cy.contains('Ver').click()
+    cy.contains('🧭').click()
+    cy.contains('Ocio').click()
+    cy.get('input[name="name"]').type('Prueba actividad')
+    cy.get('input[name="description"]').type('Prueba de actividad')
+    cy.get('input[name="startDate"]').type('2017-06-01T08:30')
+    cy.get('input[name="endDate"]').type('2017-06-01T08:31')
+    cy.get('input[name="price"]').type('1000')
+    cy.contains('Guardar').click()
+    cy.contains('Cancelar').click()
+    cy.visit('http://localhost:4200/travels')
+    cy.contains('Ver').click()
+  })
+})
+
+describe('Delete activity', () => {
+  it('should delete an activity inside a travel', () => {
+    it('Add a new activity', () => {
+      cy.setLocalStorage('token', JSON.stringify('token'))
+      cy.visit('http://localhost:4200/travels')
+      cy.contains('Ver').click()
+      cy.contains('chakra-button css-10dk087').click()
+      cy.contains('Cancelar').click()
+    })
   })
 })
